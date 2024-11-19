3498a9d0bddb6fa9abec883af09e45038ec5d1bf
Deekshitha-N <88249897+Deekshitha-N@users.noreply.github.com>
2024-11-19 21:56:02 +0530

    Update singleton-logger.cs

--- creational/singleton-logger.cs
+++ creational/singleton-logger.cs
@@ -1,36 +1,36 @@
 namespace Design_Principles_Creational_Singleton
 {
-	public sealed class Logger
+	public sealed class Logger //Not able to inherit
 	{
 		private static Logger loggerInstance;
 		private static readonly object lockObject = new object();
 		
 		private Logger()
-        {
-        }
+        	{
+        	}
 		
 		public static Logger CreateLoggerInstance
-	    {
+		{
 			get
-	        {
+			{
 				if (loggerInstance == null)
-	            {
-	                lock (lockObject)
-	                {
-	                    if (loggerInstance == null)
-	                    {
-	                        loggerInstance = new Logger();
-	                    }
-	                }
-	            }
-	
-	            return loggerInstance;
-	        }
+				{
+			                lock (lockObject)
+			                {
+			                    if (loggerInstance == null)
+			                    {
+			                        loggerInstance = new Logger();
+			                    }
+			                }
+		            	}
+		
+		            	return loggerInstance;
+		        }
 		}
-    }
+    	}
 
 	internal class Program
-    {
+    	{
 		public static void Main(string[] args)
 		{
 			Logger logger = Logger.CreateLoggerInstance;
@@ -40,6 +40,6 @@ namespace Design_Principles_Creational_Singleton
 			
 			Console.WriteLine(logger.GetHashCode());
 			Console.WriteLine(logger2.GetHashCode());
-	    }
-    }
+	    	}
+   	}
 }
