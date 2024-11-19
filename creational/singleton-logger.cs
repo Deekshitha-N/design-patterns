3eb1d12929d68e3bae4ec7efa8ba18eee14b770e
Deekshitha-N <88249897+Deekshitha-N@users.noreply.github.com>
2024-11-19 21:53:09 +0530

    Create singleton-logger.cs

---
+++ creational/singleton-logger.cs
@@ -0,0 +1,45 @@
+namespace Design_Principles_Creational_Singleton
+{
+	public sealed class Logger
+	{
+		private static Logger loggerInstance;
+		private static readonly object lockObject = new object();
+		
+		private Logger()
+        {
+        }
+		
+		public static Logger CreateLoggerInstance
+	    {
+			get
+	        {
+				if (loggerInstance == null)
+	            {
+	                lock (lockObject)
+	                {
+	                    if (loggerInstance == null)
+	                    {
+	                        loggerInstance = new Logger();
+	                    }
+	                }
+	            }
+	
+	            return loggerInstance;
+	        }
+		}
+    }
+
+	internal class Program
+    {
+		public static void Main(string[] args)
+		{
+			Logger logger = Logger.CreateLoggerInstance;
+			Logger logger2 = Logger.CreateLoggerInstance;
+
+			//Logger log = new Logger(); Not able to create instance due to private constructor
+			
+			Console.WriteLine(logger.GetHashCode());
+			Console.WriteLine(logger2.GetHashCode());
+	    }
+    }
+}
